9ff5186cf4eb89034f4b6cf0b35091b4e66124e0
joanhyokim <joan.kim@liferay.com>
2016-01-27 07:15:43 -0800

    stuff and things

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,33 @@
 function getParameterByName(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+	var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+
+	return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function getDisplayRow(name, value) {
+	var row = document.createElement("div");
+
+	row.setAttribute('class', 'row');
+
+	var nameColumn = document.createElement("div");
+
+	nameColumn.setAttribute('class', 'col-md-1');
+
+	var nameNode = document.createTextNode(name);
+
+	nameColumn.appendChild(nameNode);
+
+	row.appendChild(nameColumn);
+
+	var valueColumn = document.createElement("div");
+
+	valueColumn.setAttribute('class', 'col-md-4');
+
+	var valueNode = document.createTextNode(value);
+
+	valueColumn.appendChild(valueNode);
+
+	row.appendChild(valueColumn);
+
+	return row;
 }
